16fcf30611dd9b4a06d57d670c2d6ca8b2aeb3ff
mps9506 <mpschramm@gmail.com>
2024-09-27 14:18:49 -0400

    set test conditions

--- tests/testthat/test-download_nlcd.R
+++ tests/testthat/test-download_nlcd.R
@@ -1,4 +1,6 @@
 test_that("l48 nlcd paths are correct", {
+  testthat::skip_on_cran()
+  testthat::skip_if_offline()
   set_gdal_config('AWS_NO_SIGN_REQUEST', 'YES')
   landmass <- "l48"
   years <- c("2001", "2004", "2006", "2008", "2011", "2013", "2016", "2019", "2021")
@@ -15,6 +17,8 @@ test_that("l48 nlcd paths are correct", {
 
 
 test_that("ak nlcd paths are correct", {
+  testthat::skip_on_cran()
+  testthat::skip_if_offline()
   set_gdal_config('AWS_NO_SIGN_REQUEST', 'YES')
   landmass <- "ak"
   years <- c("2016", "2011", "2001")
