2033a050c864eda9aa6c63f0f67f8ca216e828d5
mps9506 <mpschramm@gmail.com>
2025-02-19 11:06:18 -0500

    add `download_urban_areas()`

--- R/download_urban_areas.R
+++ R/download_urban_areas.R
@@ -1,20 +1,18 @@
-#' Download U.S. Census block boundaries
+#' Download U.S. Urban Areas
 #'
-#' Downloads U.S. Census block boundaries and associated information for
-#' blocks within the spatial extent of the template SpatRaster.
+#' Downloads a U.S. Census designated urban areas intersecting the spatial extent of the template SpatRaster.
 #'
 #' @param template A SpatRaster object.
-#' @param year A character value. Any of the following values should work: `c("2000","2010","2020")`.
 #' @param page_size a numeric value passed to `arcgislayers::arcselect()`. Defaults to NULL. Useful when the requests returns a 500 error code.
 #' @param output A character file path specifying where the raster file should be stored. Defaults to a temporary file.
 #'
 #' @return A terra SpatVector object. If API resources are not available an invisible `NULL` is returned.
 #' @importFrom arcgislayers arc_open arc_select get_layer
 #' @importFrom cli cli_alert_info
-#' @importFrom rlang arg_match
 #' @importFrom sf st_bbox
 #' @importFrom terra vect writeVector
 #' @export
+#'
 #' @examples
 #' # example code
 #' \donttest{
@@ -22,14 +20,14 @@
 #' dem <- system.file("extdata", "thompsoncreek.tif", package = "SELECTRdata")
 #' dem <- terra::rast(dem)
 #'
-#' blocks <- download_census_blocks(template = dem)
-#' blocks
+#' ua <- download_urban_areas(template = dem)
+#' ua
 #' }
 #'
-download_census_blocks <- function(template,
-                                   year = "2020",
-                                   page_size = NULL,
-                                   output = tempfile(fileext = ".gpkg")) {
+download_urban_areas <- function(template,
+                                 page_size = NULL,
+                                 output = tempfile(fileext = ".gpkg")) {
+
   ## are we online?
   ## check connectivity
   if (!isTRUE(check_connectivity("services.arcgis.com"))) {
@@ -38,23 +36,7 @@ download_census_blocks <- function(template,
   ## check template if a spatraster
   check_spat_ras(template)
 
-  ## check years
-  year <- rlang::arg_match(year,
-                           values = c('2000', '2010', '2020'))
-
-  if(year == "2020") {
-    furl <- "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Census_2020_DHC_Blocks/FeatureServer"
-    id <- 1
-  }
-  if(year == "2010") {
-    message("2010 Census endpoint not yet implemented")
-    return(invisible(NULL))
-  }
-  if(year == "2000") {
-    message("2000 Census endpoint not yet implemented")
-    return(invisible(NULL))
-  }
-
+  furl <- "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Census_Urban_Areas/FeatureServer"
 
   ## check for service errors first
   msg <- catch_arcgislayer_error(furl)
@@ -62,26 +44,26 @@ download_census_blocks <- function(template,
     cli::cli_alert_info(msg[[1]])
     return(invisible(NULL))
   } else {
-    tracts_blocks <- arcgislayers::arc_open(furl)
+    feature_server <- arcgislayers::arc_open(furl)
   }
-
+  id <- 0
 
   ## this needs to be wrapped a try
-  blocks_layer <- arcgislayers::get_layer(tracts_blocks, id = id)
+  ## we also use this mutliple times, could be wrapped up in a function (download_census, download_county etc.)
+  layer <- arcgislayers::get_layer(feature_server, id = id)
 
   ## create a bbox object from DEM
   bounds <- sf::st_bbox(template)
 
   ## retrieve the cropped featuer layer
-  blocks_sf <- arcgislayers::arc_select(x = blocks_layer,
+  queried_layer <- arcgislayers::arc_select(x = layer,
                                         filter_geom = bounds,
                                         page_size = page_size)
 
-  blocks_vect <- terra::vect(blocks_sf)
+  queried_layer_vect <- terra::vect(queried_layer)
 
-  terra::writeVector(blocks_vect,
+  terra::writeVector(queried_layer_vect,
                      filename = output)
 
   return(terra::vect(output))
-
 }
