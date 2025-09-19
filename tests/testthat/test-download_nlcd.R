eb20f56d7702d383f14d48064b3e57278ea7e84b
mps9506 <mpschramm@gmail.com>
2025-09-19 16:37:31 -0400

    use charToRaw

--- tests/testthat/test-download_nlcd.R
+++ tests/testthat/test-download_nlcd.R
@@ -49,13 +49,13 @@ test_that("get_nlcd and friends correctly handle 400 and 500 web responses",{
   mock_content <- function(req) {
     httr2::response(status_code = 200,
              headers = c("Content-Type: application/xml"),
-             body = '<?xml version="1.0" encoding="UTF-8"?><ows:ExceptionReport xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:ows="http://www.opengis.net/ows/2.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="2.0.0" xsi:schemaLocation="http://www.opengis.net/ows/2.0 http://schemas.opengis.net/ows/2.0/owsExceptionReport.xsd">
+             body = charToRaw('<?xml version="1.0" encoding="UTF-8"?><ows:ExceptionReport xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:ows="http://www.opengis.net/ows/2.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="2.0.0" xsi:schemaLocation="http://www.opengis.net/ows/2.0 http://schemas.opengis.net/ows/2.0/owsExceptionReport.xsd">
   <ows:Exception exceptionCode="NoApplicableCode">
     <ows:ExceptionText>Failed to read the coverage mrlc_Land-Cover-Native_conus_year_data:Land-Cover-Native_conus_year_data
 The coordinate reference system must be the same for all objects.</ows:ExceptionText>
   </ows:Exception>
 </ows:ExceptionReport>
-')
+'))
   }
 
 
